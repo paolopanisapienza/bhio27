ca990d8e02279f62237dfb3c23000cd932c5b25e
Paolo pani <paolo.pani@uniroma1.it>
2026-09-01 15:50:17 +0200

    footer

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const footer = document.createElement("footer");
 
   footer.innerHTML = `
-    <p>Black Holes Inside and Out II · Sapienza University of Rome · 23–27 August 2027</p>
+    <p>Black Holes III · Sapienza University of Rome · 23–27 August 2027</p>
   `;
 
   document.body.appendChild(footer);
